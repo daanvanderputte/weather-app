6c8d7346bfe12d5ad115021080b2428ee78e5e0c
daanvanderputte <daanvanderputte@protonmail.com>
2022-08-10 15:27:01 +0200

    Added search bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import axios from "axios"
 
 function App() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=80996b5a8f7ac15145fdf4b600a617d2`
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=80996b5a8f7ac15145fdf4b600a617d2`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }  
+  }
 
   return (
     <div className="App">
+      <div className="search">
+        <input 
+        value={location} 
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text"/>
+
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
@@ -21,17 +42,19 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>34C</p>
+            <p className="bold">34C</p>
+            <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p className="bold">20%</p>
+            <p>Humidity</p>
           </div>
-
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
         </div>
-
-      </div>
-
-      
+      </div>     
     </div>
   );
 }
