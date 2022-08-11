68491c5be863c74bf358a188ebf9a5c6724f5df8
daanvanderputte <daanvanderputte@protonmail.com>
2022-08-11 15:25:42 +0200

    Cleanup directory and change metric system

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}`
-
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
@@ -36,7 +35,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>{data.main?.temp}˚F</h1>
+            <h1>{data.main?.temp}˚C</h1>
           </div>
           <div className="description">
             <p>{data.weather && data.weather[0].main}</p>
@@ -44,15 +43,15 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            <p className="bold">{data.main?.feels_like}˚F</p>
+            <p className="bold">{data.main?.feels_like}˚C</p>
             <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p className="bold">{data.main?.humidity}</p>
+            <p className="bold">{data.sys?.sunrise}</p>
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">{data.wind?.speed}</p>
+            <p className="bold">{data.sys?.country}</p>
             <p>Wind Speed</p>
           </div>
         </div>
